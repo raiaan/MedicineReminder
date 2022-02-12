fe68dc915f79c8fa7e53ebfa2dce23001673371c
yasmeenhosny98 <yasmeenhosny98.1@gmail.com>
2022-02-12 23:39:10 +0200

    Adding static splash screen

---
+++ app/src/main/java/com/example/mymedcine/splashScreen/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.mymedcine.splashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.example.mymedcine.MainActivity;
+import com.example.mymedcine.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        Handler handler = new Handler(Looper.myLooper());
+        handler.postDelayed(new MyRunnable(),3000);
+
+    }
+
+    class MyRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
