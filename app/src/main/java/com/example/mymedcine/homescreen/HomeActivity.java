819373c20077ffdb07c1d882ef7d946ae4eb4fb7
yasmeenhosny98 <yasmeenhosny98.1@gmail.com>
2022-02-20 15:13:52 +0200

    modifying the bottom bar navigation and set it with its fragments using navigator controller

--- app/src/main/java/com/example/mymedcine/homescreen/HomeActivity.java
+++ app/src/main/java/com/example/mymedcine/homescreen/HomeActivity.java
@@ -1,40 +1,30 @@
 package com.example.mymedcine.homescreen;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.example.mymedcine.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        bottomNavigationView = findViewById(R.id.bottom_navigation);
-//        bottomNavigationView.setOnItemSelectedListener(item ->
-//        {switch(item.getItemId()) {
-//            case R.id.home_fragment:
-//                Toast.makeText(this, "Home Page", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.medicine_fragment:
-//                Toast.makeText(this, "Medicine Page", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.taker_fragment:
-//                Toast.makeText(this, "Taker Page", Toast.LENGTH_SHORT).show();
-//                break;
-//        }
-//        return false;
-//        });
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        navController = Navigation.findNavController(this, R.id.homeNavHost);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 
 }
